8938185320d4db66964a69630ead6f0c15d1e0d1
Noé <noefarese@gmx.ch>
2026-04-04 19:42:53 +0200

    refactor GlobalExceptionHandler

--- smart-fridge-backend/src/main/kotlin/ch/bbzw/smartfridgebackend/exceptions/GlobalExceptionHandler.kt
+++ smart-fridge-backend/src/main/kotlin/ch/bbzw/smartfridgebackend/exceptions/GlobalExceptionHandler.kt
@@ -1,7 +1,6 @@
 package ch.bbzw.smartfridgebackend.exceptions
 
-import java.util.Collections;
-import org.slf4j.LoggerFactory
+import java.util.Collections
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
@@ -9,8 +8,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler
 
 @ControllerAdvice
 class GlobalExceptionHandler {
-    private val log = LoggerFactory.getLogger(GlobalExceptionHandler::class.java)
-
     @ExceptionHandler
     fun handleUserAlreadyExistsException(ex: UserAlreadyExistsException): ResponseEntity<MutableMap<String, String>> {
         return ResponseEntity
