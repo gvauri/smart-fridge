cafaa55a40b2490e4690bb6d580d73bb0d95989e
Noé <noefarese@gmx.ch>
2026-04-09 15:08:10 +0200

    remove unused import

--- smart-fridge-java-backend/src/main/java/css/ch/smartfridgejavabackend/auth/service/JwtService.java
+++ smart-fridge-java-backend/src/main/java/css/ch/smartfridgejavabackend/auth/service/JwtService.java
@@ -1,8 +1,6 @@
 package css.ch.smartfridgejavabackend.auth.service;
 
 
-import css.ch.smartfridgejavabackend.user.dto.UserResponseDTO;
-
 import javax.crypto.SecretKey;
 
 public interface JwtService {
