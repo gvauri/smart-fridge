b8c22d48fead813d391e2280c0b638f91164651a
p20669 <aurel.schmid@css.ch>
2026-04-23 14:13:14 +0200

    update and delete

--- smart-fridge-frontend/src/app/component/items/item/item.ts
+++ smart-fridge-frontend/src/app/component/items/item/item.ts
@@ -3,7 +3,9 @@ import {Item} from '../../../shared/models/item/item.model';
 import {DatePipe, NgOptimizedImage} from '@angular/common';
 import {MatIcon} from '@angular/material/icon';
 import {MatIconButton} from '@angular/material/button';
+import {MatDialog} from '@angular/material/dialog';
 import {ItemService} from '../../../shared/services/item.service';
+import {UpdateItem} from '../../update-item/update-item';
 
 @Component({
   selector: 'app-item',
@@ -18,10 +20,15 @@ import {ItemService} from '../../../shared/services/item.service';
 })
 export class ItemComponent {
   private readonly itemService = inject(ItemService);
+  private readonly dialog = inject(MatDialog);
 
   readonly item = input.required<Item>();
 
-  protected delete() {
-    this.itemService.deleteItem(this.item().id)
+  protected openUpdateDialog(): void {
+    this.dialog.open(UpdateItem, {data: this.item()});
+  }
+
+  protected delete(): void {
+    this.itemService.deleteItem(this.item().id).subscribe();
   }
 }
---
+++ smart-fridge-frontend/src/app/component/update-item/update-item.ts
@@ -0,0 +1,78 @@
+import {Component, inject, signal} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogActions, MatDialogContent, MatDialogRef, MatDialogTitle} from '@angular/material/dialog';
+import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
+import {MatFormField, MatInput, MatLabel, MatSuffix} from '@angular/material/input';
+import {MatOption, MatSelect} from '@angular/material/select';
+import {MatButton} from '@angular/material/button';
+import {MatDatepicker, MatDatepickerInput, MatDatepickerToggle} from '@angular/material/datepicker';
+import {ItemService} from '../../shared/services/item.service';
+import {ErrorMessageService} from '../../shared/services/error-message.service';
+import {IconType} from '../../shared/models/item/item-icon.type';
+import {Item} from '../../shared/models/item/item.model';
+import {MessageBox} from '../message-box/message-box';
+import {MessageType} from '../../shared/models/message.type';
+
+@Component({
+  selector: 'app-update-item',
+  imports: [
+    MatDialogContent,
+    ReactiveFormsModule,
+    MatFormField,
+    MatLabel,
+    MatInput,
+    MatSelect,
+    MatSuffix,
+    MatOption,
+    MatDialogActions,
+    MatButton,
+    MatDialogTitle,
+    MatDatepicker,
+    MatDatepickerToggle,
+    MatDatepickerInput,
+    MessageBox,
+  ],
+  templateUrl: './update-item.html',
+  styleUrl: './update-item.scss',
+})
+export class UpdateItem {
+  protected errorMessage = signal<string>('');
+  protected hasErrorOccurred = signal<boolean>(false);
+  private readonly service = inject(ItemService);
+  private readonly errorService = inject(ErrorMessageService);
+  private readonly dialogRef = inject(MatDialogRef<UpdateItem>);
+  private readonly item = inject<Item>(MAT_DIALOG_DATA);
+  private readonly fb = inject(FormBuilder);
+  protected readonly MessageType = MessageType;
+  protected readonly iconTypes = Object.values(IconType);
+  protected readonly formUpdate = this.fb.group({
+    name: [this.item.name, Validators.required],
+    description: [this.item.description, Validators.required],
+    icon: [this.item.icon as IconType | null, Validators.required],
+    amount: [this.item.amount, [Validators.required, Validators.min(1)]],
+    expirationDate: [this.item.expirationDate as Date | null, Validators.required],
+  });
+
+  protected submit(): void {
+    if (this.formUpdate.valid) {
+      this.service.updateItem(this.item.id, {
+        icon: this.formUpdate.get('icon')?.value ?? IconType.OTHER,
+        name: this.formUpdate.get('name')?.value ?? '',
+        description: this.formUpdate.get('description')?.value ?? '',
+        expirationDate: this.formUpdate.get('expirationDate')?.value ?? new Date(),
+        amount: this.formUpdate.get('amount')?.value ?? 1,
+      }).subscribe({
+        next: () => this.cancel(),
+        error: (error) => {
+          this.hasErrorOccurred.set(true);
+          this.errorMessage.set(this.errorService.getErrorMessage(error));
+        }
+      });
+    } else {
+      this.formUpdate.markAllAsTouched();
+    }
+  }
+
+  protected cancel(): void {
+    this.dialogRef.close();
+  }
+}
